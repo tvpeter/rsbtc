3488a3fee4789742cef889c22581eb42da1d33fe
Vihiga Tyonum <withtvpeter@gmail.com>
2024-12-20 22:03:00 +0100

    add verifying and signing methods

--- lib/src/crypto.rs
+++ lib/src/crypto.rs
@@ -1,9 +1,28 @@
-use ecdsa::{signature::Signer, Signature as ECDSASignature, SigningKey, VerifyingKey};
+use crate::sha256::Hash;
+use ecdsa::signature::Verifier;
+use ecdsa::signature::Signer;
+use ecdsa::{Signature as ECDSASignature, SigningKey, VerifyingKey};
 use k256::Secp256k1;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Signature(ECDSASignature<Secp256k1>);
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Signature(pub ECDSASignature<Secp256k1>);
+
+impl Signature {
+    // sign a crate::types::TransactionOutput from its sha256 hash
+    pub fn sign_output(output_hash: &Hash, private_key: &PrivateKey) -> Self {
+        let signing_key = &private_key.0;
+        let signature: ECDSASignature<Secp256k1> = signing_key.sign(&output_hash.as_bytes());
+        Signature(signature)
+    }
+
+    pub fn verify(&self, output_hash: &Hash, public_key: &PublicKey) -> bool {
+        public_key
+            .0
+            .verify(&output_hash.as_bytes(), &self.0)
+            .is_ok()
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct PublicKey(VerifyingKey<Secp256k1>);
@@ -40,6 +59,6 @@ impl PrivateKey {
     }
 
     pub fn public_key(&self) -> PublicKey {
-        PublicKey(self.0.verifying_key().clone())
+        PublicKey(*self.0.verifying_key())
     }
 }
