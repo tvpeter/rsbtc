aad9e0d75d33cd1a204ad43bb60ba25886eb7468
Vihiga Tyonum <withtvpeter@gmail.com>
2024-12-31 10:28:04 +0100

    add saveable trait

--- lib/src/util.rs
+++ lib/src/util.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs::File,
+    io::{Read, Result as IoResult, Write},
+    path::Path,
+};
+
 use serde::{Deserialize, Serialize};
 
 use crate::{sha256::Hash, types::Transaction};
@@ -25,5 +31,21 @@ impl MerkleRoot {
             layer = new_layer;
         }
         MerkleRoot(layer[0])
+    }
+}
+
+pub trait Saveable
+where
+    Self: Sized,
+{
+    fn load<I: Read>(reader: I) -> IoResult<Self>;
+    fn save<O: Write>(&self, writer: O) -> IoResult<()>;
+    fn save_to_file<P: AsRef<Path>>(&self, path: P) -> IoResult<()> {
+        let file = File::create(&path)?;
+        self.save(file)
+    }
+    fn load_from_file<P: AsRef<Path>>(path: P) -> IoResult<Self> {
+        let file = File::open(&path)?;
+        Self::load(file)
     }
 }
