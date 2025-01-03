89fc9c44906c5c9966051f098ffadf149276ec64
Vihiga Tyonum <withtvpeter@gmail.com>
2025-01-03 22:00:04 +0100

    add mining a block

--- lib/src/bin/tx_gen.rs
+++ lib/src/bin/tx_gen.rs
@@ -1,6 +1,11 @@
 use std::{env, process::exit};
 
-use btc_lib::{crypto::PrivateKey, types::{Transaction, TransactionOutput}, util::Saveable, INITIAL_REWARD};
+use btc_lib::{
+    crypto::PrivateKey,
+    types::{Transaction, TransactionOutput},
+    util::Saveable,
+    INITIAL_REWARD,
+};
 use uuid::Uuid;
 
 fn main() {
@@ -13,12 +18,15 @@ fn main() {
 
     let private_key = PrivateKey::new_key();
 
-    let transaction = Transaction::new(vec![], vec![
-        TransactionOutput {
+    let transaction = Transaction::new(
+        vec![],
+        vec![TransactionOutput {
             unique_id: Uuid::new_v4(),
             value: INITIAL_REWARD * 10u64.pow(8),
             pubkey: private_key.public_key(),
         }],
     );
-    transaction.save_to_file(path).expect("failed to save transaction");
+    transaction
+        .save_to_file(path)
+        .expect("failed to save transaction");
 }
--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,3 +1,35 @@
+use std::{env, process::exit, usize};
+
+use btc_lib::{types::Block, util::Saveable};
+
 fn main() {
-    println!("Hello, world!");
+    // parse block path and the steps count from the first and
+    // second argument
+    let (path, steps) = if let (Some(arg), Some(arg2)) = (env::args().nth(1), env::args().nth(2)) {
+        (arg, arg2)
+    } else {
+        eprintln!("usage: miner <block_file> <steps>");
+        exit(1);
+    };
+
+    let steps = if let Ok(s @ 1..=usize::MAX) = steps.parse() {
+        s
+    } else {
+        eprintln!("<steps> should be a positive integer");
+        exit(1);
+    };
+
+    // load block from file
+    let og_block = Block::load_from_file(path).expect("failed to load block");
+    let mut block = og_block.clone();
+
+    while !block.header.mine(steps) {
+        println!("mining...");
+    }
+
+    println!("original: {:#?}", og_block);
+    println!("hash: {}", og_block.header.hash());
+    //mined block
+    println!("final: {:#?}", block);
+    println!("hash: {}", block.header.hash());
 }
