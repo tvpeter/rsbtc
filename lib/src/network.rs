ee594804a2af6361faf1dca0abf0f8face919356
Vihiga Tyonum <withtvpeter@gmail.com>
2025-01-03 23:07:52 +0100

    add impl for message

--- lib/src/network.rs
+++ lib/src/network.rs
@@ -1,8 +1,12 @@
+use serde::{Deserialize, Serialize};
+
 use crate::{
     crypto::PublicKey,
     types::{Block, Transaction, TransactionOutput},
 };
+use std::io::{Error as IoError, Read, Write};
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum Message {
     /// fetch all utxos belonging to a public key
     FetchUTXOs(PublicKey),
@@ -13,10 +17,10 @@ pub enum Message {
     /// Broadcast a new transaction to other nodes
     NewTransaction(Transaction),
     /// Ask the node to prepare the optimal block template
-    /// with the coinbase transaction paying the specified 
+    /// with the coinbase transaction paying the specified
     /// public key
     FetchTemplate(PublicKey),
-    /// the template 
+    /// the template
     Template(Block),
     /// Ask the node to valide a block template
     /// this is to prevent the node from mining an invalid block
@@ -25,17 +29,46 @@ pub enum Message {
     TemplateValidity(bool),
     /// submit a mined block to a node
     SubmitTemplate(Block),
-    /// Ask a node to report all the other nodes it knows about 
+    /// Ask a node to report all the other nodes it knows about
     DiscoverNodes,
     /// This is the response to DiscoverNodes
     NodeList(Vec<String>),
-    /// Ask a node whats the higheest block it knows about in comparison 
-    /// to the local blockchain 
+    /// Ask a node whats the higheest block it knows about in comparison
+    /// to the local blockchain
     AskDifference(u32),
-    /// This is the response to AskDifference 
+    /// This is the response to AskDifference
     Difference(i32),
     /// Ask a node to send a block with the specified height
     FetchBlock(usize),
     /// Broadcast a new block to other nodes
     NewBlock(Block),
+}
+
+impl Message {
+    pub fn encode(&self) -> Result<Vec<u8>, ciborium::ser::Error<IoError>> {
+        let mut bytes = Vec::new();
+        ciborium::into_writer(self, &mut bytes)?;
+        Ok(bytes)
+    }
+
+    pub fn decode(data: &[u8]) -> Result<Self, ciborium::de::Error<IoError>> {
+        ciborium::from_reader(data)
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> Result<(), ciborium::ser::Error<IoError>> {
+        let bytes = self.encode()?;
+        let len = bytes.len() as u64;
+        stream.write_all(&len.to_be_bytes())?;
+        stream.write_all(&bytes)?;
+        Ok(())
+    }
+
+    pub fn receive(stream: &mut impl Read) -> Result<Self, ciborium::de::Error<IoError>> {
+        let mut len_bytes = [0u8; 8];
+        stream.read_exact(&mut len_bytes)?;
+        let len = u64::from_be_bytes(len_bytes) as usize;
+        let mut data = vec![0u8; len];
+        stream.read_exact(&mut data)?;
+        Self::decode(&data)
+    }
 }
