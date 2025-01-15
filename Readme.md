378c40a20c7fe3ecbaa910776fb45b33d81d7021
Vihiga Tyonum <withtvpeter@gmail.com>
2025-01-15 21:28:59 +0100

    add generating keys

--- Readme.md
+++ Readme.md
@@ -6,3 +6,5 @@
 - To print transaction: `cargo run --bin tx_print <generated_file>`
 - To generate block: `cargo run --bin block_gen <filename>`
 - To print block: `cargo run --bin block_print <filename>`
+- To generate keys: `cargo run --bin key_gen ./miner/<keyname> `
+- To mine using generated keys: ` cargo run --bin miner localhost::9000 ./miner/alice.pub.pem`
---
+++ lib/src/bin/key_gen.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+use btc_lib::{crypto::PrivateKey, util::Saveable};
+
+fn main (){
+    let name = env::args().nth(1).expect("please provide a name");
+
+    let private_key = PrivateKey::new_key();
+    let public_key = private_key.public_key();
+
+    let public_key_file = name.clone() + ".pub.pem";
+    let private_key_file = name.clone() + "priv.cbor";
+    private_key.save_to_file(&private_key_file).unwrap();
+    public_key.save_to_file(&public_key_file).unwrap();
+}
